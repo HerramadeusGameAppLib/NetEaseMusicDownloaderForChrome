4308e39b1b40f42d4afb9ac7973828a588e370a4
HerramadeusGameAppLib <41694742+HerramadeusGameAppLib@users.noreply.github.com>
2019-03-28 23:46:05 +0800

    Update get_song_info_from_page.js

--- script/js/get_song_info_from_page.js
+++ script/js/get_song_info_from_page.js
@@ -7,8 +7,8 @@ function onShouldSendSongInfo(message, sender, sendResponse)
     if(null == message)
         return;
     
-	var msgName = message["MsgName"];
-	if("MsgFindSongInfoForPage" != msgName)
+    var msgName = message["MsgName"];
+    if("MsgFindSongInfoForPage" != msgName)
         return;
         
     var songId = message["SongId"];
@@ -35,19 +35,19 @@ function onShouldSendSongInfo(message, sender, sendResponse)
 
 function acquireSongInfo()
 {
-	var songInfo = null;
-	var iframe = document.getElementById("g_iframe").contentDocument;
-	if(null != iframe)
-	{
- 	    var keywords = iframe.getElementsByName("keywords");
-	    if(0 != keywords.length)
-	    {
-	    	songInfo = keywords[0].getAttribute("content");
-	    }
-	}
-
+    var songInfo = null;
+    var iframe = document.getElementById("g_iframe").contentDocument;
+    if(null != iframe)
+    {
+        var keywords = iframe.getElementsByName("keywords");
+        if(0 != keywords.length)
+        {
+            songInfo = keywords[0].getAttribute("content");
+        }
+    }
+    
     if(null == songInfo)
         alert("歌曲页面结构更新了，需要更新当前算法！\n请联系开发者:herramadeus@163.com");
     
-	return songInfo;
+    return songInfo;
 }
